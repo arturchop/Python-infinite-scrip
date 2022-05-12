df24fd900fe834e2eb271cc150356b32f40ebc01
arturchop <98663820+arturchop@users.noreply.github.com>
2022-05-12 16:09:33 +0100

    Add files via upload

---
+++ repeat james.py
@@ -0,0 +1,9 @@
+
+# Initialize counter
+counter = 1
+# Iterate the loop 5 times
+while counter < 999999999999999999999999999999999999999999999999999999999999999:
+    # Print the counter value
+    print ("Amount of times James Rice has not seen his balls: %d" % counter)
+    # Increment the counter
+    counter = counter + 764387548765475428765475
